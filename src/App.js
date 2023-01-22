333406acdf4685a37b6d64b7ca061025b4bca390
Jimitriev <mgc778249@gmail.com>
2023-01-22 12:01:37 -0800

    Mercedes

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css'
+
+function App() {
+  return (
+      <div className="App">
+        Mercedes
+      </div>
+  );
+}
+
+export default App;
